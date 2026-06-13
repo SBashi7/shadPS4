3b0e39eb40d7735a7d42ddf5308a6a5489a225b9
SBashi7 <sbashi7@users.noreply.github.com>
2026-06-13 14:52:44 +0200

    fix: init MemoryWatcher + hardcode texture_audit ON

--- src/video_core/knack_render_diag.cpp
+++ src/video_core/knack_render_diag.cpp
@@ -58,6 +58,11 @@ Flags Flags::LoadFromEnv() {
     // Read watch config
     g_flags.watch_addr = EnvU64(ENV_WATCH_ADDR, 0x2a8ea0000);
     g_flags.watch_size = EnvU32(ENV_WATCH_SIZE, 0xE10000);
+    MemoryWatcher::Instance().Init(g_flags.watch_addr, g_flags.watch_size);
+
+    // KNACK TEXTURE AUDIT: hardcoded ON
+    g_flags.texture_audit = true;
+    g_flags.effect_diag = true;
 
     // NEVER write files during startup. FrameImageTracker only uses LOG_INFO.
     return f;
